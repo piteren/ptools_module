f523f4b6d152470d3b474d737cadcd01f6786c6f
Piotr Niewinski <me@piotrniewinski.com>
2022-04-22 11:48:18 +0200

    rm helpers

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup, find_packages
 
-from helpers import get_requirements
+# reads requirements from 'ptools/requirements.txt'
+def get_requirements():
+    with open('ptools/requirements.txt') as file:
+        lines = [l[:-1] for l in file.readlines()]
+        return lines
 
 
 setup(
